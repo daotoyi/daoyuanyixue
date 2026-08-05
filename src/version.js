35a64ca53981522e6890190baee123810b02e72a
daotoyi <wenhuas.shi@gmail.com>
2026-08-05 21:06:35 +0800

    chore: v1.9.9 版本同步

--- src/version.js
+++ src/version.js
@@ -2,8 +2,8 @@
  * 自动生成: 版本与 Git 保持一致 (构建前由 scripts/gen-version.js 更新)
  * 请勿手动修改
  */
-export const APP_VERSION = 'v1.9.8'
-export const APP_COMMIT = '685b71d'
+export const APP_VERSION = 'v1.9.9'
+export const APP_COMMIT = 'a44dac3'
 export const APP_BRANCH = 'main'
 export const APP_BUILD_DATE = '2026-08-05'
-export const APP_FULL_VERSION = 'v1.9.8 (685b71d)'
+export const APP_FULL_VERSION = 'v1.9.9 (a44dac3)'
