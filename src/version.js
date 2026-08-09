526f1a0eb9507d665a54f1a11daa06adc8a8c0ea
daotoyi <wenhuas.shi@gmail.com>
2026-08-09 20:29:47 +0800

    v1.11.27: 我的页面去掉等级说明小字,点击VIP徽章弹出等级说明

--- src/version.js
+++ src/version.js
@@ -2,8 +2,8 @@
  * 自动生成: 版本与 Git 保持一致 (构建前由 scripts/gen-version.js 更新)
  * 请勿手动修改
  */
-export const APP_VERSION = 'v1.11.27'
-export const APP_COMMIT = '30438c3'
+export const APP_VERSION = 'v1.11.28'
+export const APP_COMMIT = '431b659'
 export const APP_BRANCH = 'main'
 export const APP_BUILD_DATE = '2026-08-09'
-export const APP_FULL_VERSION = 'v1.11.27 (30438c3)'
+export const APP_FULL_VERSION = 'v1.11.28 (431b659)'
