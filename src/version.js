1da88a5c8f9a2806e8559baf47b405aba5bb0c2d
daotoyi <wenhuas.shi@gmail.com>
2026-08-20 07:59:31 +0800

    v1.11.176: 我的元宝页面新增充值记录(订单号/时间/金额/状态)

--- src/version.js
+++ src/version.js
@@ -2,8 +2,8 @@
  * 自动生成: 版本与 Git 保持一致 (构建前由 scripts/gen-version.js 更新)
  * 请勿手动修改
  */
-export const APP_VERSION = 'v1.11.175'
-export const APP_COMMIT = '6c06900'
+export const APP_VERSION = 'v1.11.176'
+export const APP_COMMIT = 'a4063da'
 export const APP_BRANCH = 'main'
 export const APP_BUILD_DATE = '2026-08-19'
-export const APP_FULL_VERSION = 'v1.11.175 (6c06900)'
+export const APP_FULL_VERSION = 'v1.11.176 (a4063da)'
