c3c32b1e70f40ca64d8c10dbdd3261c9535edb41
daotoyi <wenhuas.shi@gmail.com>
2026-08-31 14:54:19 +0800

    v1.11.271: 版本文件+gradle同步

--- src/version.js
+++ src/version.js
@@ -2,8 +2,8 @@
  * 自动生成: 版本与 Git 保持一致 (构建前由 scripts/gen-version.js 更新)
  * 请勿手动修改
  */
-export const APP_VERSION = 'v1.11.270'
-export const APP_COMMIT = '2b66d90'
+export const APP_VERSION = 'v1.11.271'
+export const APP_COMMIT = 'af567c7'
 export const APP_BRANCH = 'main'
 export const APP_BUILD_DATE = '2026-08-31'
-export const APP_FULL_VERSION = 'v1.11.270 (2b66d90)'
+export const APP_FULL_VERSION = 'v1.11.271 (af567c7)'
