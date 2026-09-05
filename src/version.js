d66b8fa850ba4709e2fd49dac16d07e93ec8c7c7
daotoyi <wenhuas.shi@gmail.com>
2026-09-05 12:39:06 +0800

    chore: 写入 v1.12.8 版本元数据 (commit 6700c05)

--- src/version.js
+++ src/version.js
@@ -3,7 +3,7 @@
  * 请勿手动修改
  */
 export const APP_VERSION = 'v1.12.8'
-export const APP_COMMIT = '9aaa503'
+export const APP_COMMIT = '6700c05'
 export const APP_BRANCH = 'main'
 export const APP_BUILD_DATE = '2026-09-05'
-export const APP_FULL_VERSION = 'v1.12.7 (9aaa503)'
+export const APP_FULL_VERSION = 'v1.12.8 (6700c05)'
