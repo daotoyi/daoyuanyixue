6bd4f2ae4e3bac3fb13afe25d6301395a2ea08a9
daotoyi <wenhuas.shi@gmail.com>
2026-09-08 13:47:33 +0800

    build: 同步 version.js APP_COMMIT 为发布提交 72dd42c (v1.12.23 收尾)

--- src/version.js
+++ src/version.js
@@ -3,7 +3,7 @@
  * 请勿手动修改
  */
 export const APP_VERSION = 'v1.12.23'
-export const APP_COMMIT = '34dc39a'
+export const APP_COMMIT = '72dd42c'
 export const APP_BRANCH = 'main'
 export const APP_BUILD_DATE = '2026-09-08'
-export const APP_FULL_VERSION = 'v1.12.23 (34dc39a)'
+export const APP_FULL_VERSION = 'v1.12.23 (72dd42c)'
