4aff6bc95b1f7351ac2d4751a59407e6ce0d9445
daotoyi <wenhuas.shi@gmail.com>
2026-09-09 22:22:28 +0800

    chore: 同步 version.js APP_COMMIT 为发布提交 c4ebd0f (v1.12.30 纠偏)

--- src/version.js
+++ src/version.js
@@ -3,7 +3,7 @@
  * 请勿手动修改
  */
 export const APP_VERSION = 'v1.12.30'
-export const APP_COMMIT = 'c701ae4'
+export const APP_COMMIT = 'c4ebd0f'
 export const APP_BRANCH = 'main'
-export const APP_BUILD_DATE = '2026-09-08'
-export const APP_FULL_VERSION = 'v1.12.29 (c701ae4)'
+export const APP_BUILD_DATE = '2026-09-09'
+export const APP_FULL_VERSION = 'v1.12.30 (c4ebd0f)'
